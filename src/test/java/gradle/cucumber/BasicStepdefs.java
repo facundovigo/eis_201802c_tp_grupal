4e879294fffcd5ff83236f6b89a7c4ca543bbbfa
Enrique <alonso.em@gmail.com>
2018-10-14 10:07:42 -0300

    Agrego fallo

--- src/test/java/gradle/cucumber/BasicStepdefs.java
+++ src/test/java/gradle/cucumber/BasicStepdefs.java
@@ -2,10 +2,13 @@ package gradle.cucumber;
 
 import cucumber.api.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BasicStepdefs {
 
     @When("^I run a failing step")
     public void I_run_a_failing_step() throws Throwable {
         new Production().doWork();
+        assertEquals(1,2);
     }
 }
